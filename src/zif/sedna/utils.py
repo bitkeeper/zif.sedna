653da8ba7285fd4badd9070d0ac5df338762d45c
jwashin <jwashin@bbf42d97-aa24-0410-938a-f5bdb5c61aae>
2008-03-21 15:45:55 +0000

    added a few more methods

--- src/zif/sedna/utils.py
+++ src/zif/sedna/utils.py
@@ -1,19 +1,16 @@
-#sednautils
+#sednaobject
 from lxml import objectify
 from lxml.etree import _Element, tounicode, tostring,fromstring, XMLSyntaxError
 from lxml.doctestcompare import norm_whitespace
+from dbapiexceptions import DatabaseError
 
 def checkobj(obj):
     if isinstance(obj,_Element):
         item = tounicode(obj)
-    elif isinstance(obj,basestring):
-        try:
-            test = fromstring(obj)
-        except XMLSyntaxError:
-            raise ValueError ('expected XML, got %s' % obj)
-        item = obj
+    elif isinstance(obj,SednaElement):
+        item = str(obj)
     else:
-        raise ValueError ('expected XML, got %s' % obj)
+        item = obj
     return item
 
 class SednaXPath(object):
@@ -28,7 +25,7 @@ class SednaXPath(object):
         self._count = None
         self._attrib = None
 
-    def count(self,tag=None):
+    def count(self):
         if self._count is not None:
             return self._count
         q = u'let $i := %s' % (self.path)
@@ -59,6 +56,40 @@ class SednaXPath(object):
         if key < 0:
             key += self.count()
         return self[key:key+1][0]
+
+    def index(self,obj):
+        item = checkobj(obj)
+        normed = norm_whitespace(item)
+        q = u'declare namespace se = "http://www.modis.ispras.ru/sedna";'
+        q += 'declare option se:output "indent=no";'
+        q += u' %s' % self.path
+        s = self.cursor.execute(q)
+        count = -1
+        found = False
+        for k in s:
+            count += 1
+            if norm_whitespace(k) == normed:
+                found = True
+                break
+        if found:
+            return count
+        else:
+            raise ValueError('item not in list')
+
+    def xenumerate(self):
+        for idx,value in enumerate(self):
+            yield idx+1, value
+
+    def xindex(self,obj):
+        return self.index(obj) + 1
+
+    def __contains__(self,obj):
+        try:
+            s = self.index(obj)
+            return True
+        except ValueError:
+            return False
+
 
     def __getslice__(self,start,stop):
         #start,stop,step = key.indices(self.count())
@@ -94,27 +125,48 @@ class SednaElement(SednaXPath):
     initialize with a cursor and a path to the element.
 
     """
-    def __init__(self,cursor,path):
+    def __init__(self,cursor,path,check=True):
+        """
+        init the class with cursor and path
+        set check to false to eliminate a server request, but only if you
+        know what you are doing...
+        """
         super(SednaElement,self).__init__(cursor,path)
-        self._checkElement()
+        if check:
+            self._checkElement()
 
     def _checkElement(self):
+        """
+        do a check to see that this is a single element
+        """
         q = u'let $i := %s' % (self.path,)
         q += u' return <i><c>{count($i)}</c></i>'
         s = self.cursor.execute(q)
         f = objectify.fromstring(s.value)
         c = int(f.c)
         if c == 1:
-            return 
+            return
         elif c == 0:
             raise ValueError(
             'The path did not return an element. ([0] might need to be [1]?)')
         else:
             raise ValueError(
-        'Cannot init SednaElement with a query that returns multiple elements.')
+        'Cannot init SednaElement with multiple elements.')
+
+    def getparent(self):
+        """
+        return parent as a SednaElement or None if at root
+        """
+        c = self.path + '/..'
+        t = SednaElement(self.cursor,c)
+        if t.tag is None:
+            return None
+        return t
+
+    parent = property(getparent)
 
     def count(self,tag=None):
-        if self._count:
+        if self._count is not None and tag is None:
             return self._count
         if tag:
             pt = tag
@@ -153,7 +205,7 @@ class SednaElement(SednaXPath):
         if found:
             return count
         else:
-            raise NotFoundError('item not in list')
+            raise ValueError('item not in list')
 
     def extend(self,items):
         for item in items:
@@ -178,8 +230,6 @@ class SednaElement(SednaXPath):
         else:
             self.append(item)
 
-    def xindex(self,obj):
-        return self.index(obj) + 1
 
     def remove(self,obj):
         index = self.index(obj) + 1
@@ -200,9 +250,13 @@ class SednaElement(SednaXPath):
 
     @property
     def tag(self):
-        t = self.path.split('/')[-1]
-        t1 = t.split('[')[0]
-        return t1.strip()
+        q = u"let $i := %s return <t>{$i/name()}</t>" % self.path
+        t1 = self.cursor.execute(q)
+        r = fromstring(t1.value)
+        return r.text
+        #t = self.path.split('/')[-1]
+        #t1 = t.split('[')[0]
+        #return t1.strip()
 
     def __getitem__(self,key):
         """we get the item at index inside the element"""
@@ -218,19 +272,15 @@ class SednaElement(SednaXPath):
         s = self.cursor.execute(q)
         return self.cursor.fetchone()
 
-    def __contains__(self,obj):
-        try:
-            s = self.index(obj)
-            return True
-        except ValueError:
-            return False
-
     def __setitem__(self,key,value):
         item = checkobj(value)
         local = self._localKey(key)
         q = u'update replace $i in %s/*[%s] ' % (self.path,local)
         q += ' with %s' % item
-        s = self.cursor.execute(q)
+        try:
+            s = self.cursor.execute(q)
+        except DatabaseError:
+            raise ValueError('Update failed.  Is the item well-formed?')
 
     def set(self,key,value):
         q = u'%s' % self.path
